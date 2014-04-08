c8b0429266ad2500b9c90b45a763d3ad7e438025
Pete Donnell <pete.donnell@port.ac.uk>
2014-04-08 17:38:30 +0100

    Got Sage and Python working together: Various type conversions were required, between Python lists, Numpy arrays, Sage matrices
    Commented out testing code in Python library files
    Tidied up code conventions and added headers

--- generate_vectors.py
+++ generate_vectors.py
@@ -11,21 +11,22 @@
 #
 # @author Pete Donnell <pete dot donnell at port dot ac dot uk>
 # @copyright University of Portsmouth 2014
-# @date 01/04/2014
+# @date 08/04/2014
 ##
 
 import copy
 import itertools
+import numpy
 
 
 ##
 # Increment a lexicographically-ordered vector
 #
-# @param   vector   list     Vector to increment
-# @param   limit    int      Maximum value allowed in each entry of the vector
-# @param   offset   int      Offset value for each entry of the vector
-# @param   step     numeric  Step size to increment by - beware float issues when not an int
-# @return  success  bool     True if vector was successfully incremented, False if the vector was already its maximum value
+# @param   vector   numpy.array  Vector to increment
+# @param   limit    int          Maximum value allowed in each entry of the vector
+# @param   offset   int          Offset value for each entry of the vector
+# @param   step     numeric      Step size to increment by - beware float issues when not an int
+# @return  success  bool         True if vector was successfully incremented, False if the vector was already its maximum value
 ##
 
 def increment_vector( vector, limit, offset = 0, step = 1 ):
@@ -52,27 +53,31 @@ def increment_vector( vector, limit, offset = 0, step = 1 ):
 # Examples: To generate all possible sets of binary vectors in 5D,
 # call with generate_vectors( 5, 1, 0 ). To generate all possible
 # sets of { -1, 0, 1, 2 }-vectors in 3D, call with
-# generate_vectors( 3, 3, -1 )
+# generate_vectors( 3, 3, -1 ).
 #
-# @param   dimension    int   Dimension of the vectors to generate
-# @param   base         int   Maximum value allowed in each vector
-# @param   offset       int   Offset value for each entry of each vector
-# @param   step         int   Step size to use when generating vectors
-# @return  all_vectors  list  List of all possible sets of vectors
+# @param   dimension    int                Dimension of the vectors to generate
+# @param   base         int                Maximum value allowed in each vector
+# @param   offset       int                Offset value for each entry of each vector
+# @param   step         int                Step size to use when generating vectors
+# @return  all_vectors  list<numpy.array>  List of all possible sets of vectors
 ##
 
 def generate_vectors( dimension, base, offset = 0, step = 1 ):
-	vector = [offset] * dimension
-	# Need to use copy here as lists are mutable and hence passed by reference
-	all_vectors = [copy.copy( vector )]
-	while increment_vector( vector, base + offset, offset, step ):
+	vector = numpy.array( [offset] * dimension )
+	all_vectors = []
+	if numpy.count_nonzero( vector ) > 0:
+		# Need to use copy here as NumPy arrays are mutable and hence passed by reference
 		all_vectors.append( copy.copy( vector ) )
+	while increment_vector( vector, base + offset, offset, step ):
+		if numpy.count_nonzero( vector ) > 0:
+			all_vectors.append( copy.copy( vector ) )
 	return all_vectors
 
-
+'''
 # Generate all possible vectors, then all possible n-tuples of them using itertools combinatorics
 all_vectors = generate_vectors( dimension = 2, base = 1, offset = 0 )
 print(all_vectors)
 number_of_vectors = 4
 vectors = [vector for vector in itertools.combinations( all_vectors, number_of_vectors )]
 print( vectors )
+'''
